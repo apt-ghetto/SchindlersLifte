f8f888736414a78d510d655e2eedc73c227bdcca
Patrick Steiner <pedisteiner@msn.com>
2018-01-10 11:55:41 +0100

    In Aufgabe B kopiert

--- Liftsumulator_Basic_V1_AufgabeB/main.c
+++ Liftsumulator_Basic_V1_AufgabeB/main.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Program:   Lift simulation Basic Structure
-* Filename:  LiftSimulator_Task_ABasic
+* Filename:  LiftSimulator_Task_BBasic
 * Author:    Werner Odermatt
 * Version:   1.0
 * Date:	     09.12.2016
@@ -67,67 +67,122 @@ void UpdateDisplay (LiftPosType elevatorState);
 *******************************************************************************/
 int main(void)
 {
-  InitializePorts();  // Initialization of ports
-  InitializeStart();  // Set start state of the system
+	InitializePorts();  // Initialization of ports
+	InitializeStart();  // Set start state of the system
 
-  // Endless loop
-  while(1)
-  {
-    // Handling state machine
-    switch (state)
-    {
-      case Uninitialized:
-      {
-        // Lift position calibration to ground floor (Floor0)
+	// Endless loop
+	while(1)
+	{
+		// do always
+		UpdateDisplay(currentElevatorState);  // Update the 7-Seg. display (lift)
+		currentElevatorState = ReadElevatorState();
+		SetOutput();               // Send the calculated output values to the ports
 
-        break;
-      }
-
-
-      case Waiting:
-      {
-        // Waiting for new floor request
-
-        break;
-      }
-
-
-      case CloseDoor:
-      {
-        // Close the door and wait until the door is closed
-
-        break;
-      }
+		// Handling state machine
+		switch (state)
+		{
+			case Uninitialized:
+			{
+				// Lift position calibration to ground floor (Floor0)
+				if (ReadElevatorState() != Floor0)
+				{
+					CalibrateElevatorPosition();
+				}
+				else
+				{
+					state = OpenDoor;
+					currentElevatorState = ReadElevatorState();
+				}
+				break;
+			}
 
 
-      case MoveLift:
-      {
-        // Move cabin to the requested floor
+			case Waiting:
+			{
+				ButtonType key;
+				key = CheckKeyEvent();
+				// Waiting for new floor request
+				if (EmergencyButton != key)
+				{
+					// button was pressed
+					requestedElevatorPosition = ConvertButtonTypeToLiftPosType(key);
+					int result = currentElevatorState - requestedElevatorPosition;
+					if (result != 0)
+					{
+						elevatorDirection = result < 0 ? Up : Down;
+						if (key < 16)
+						{
+							SetIndicatorElevatorState(requestedElevatorPosition);
+						}
+						else
+						{
+							SetIndicatorFloorState(requestedElevatorPosition);
+						}
+						state = CloseDoor;
+					}
+					
+				}
 
-        break;
-      }
+				break;
+			}
 
 
-      case OpenDoor:
-      {
-        // Open the door and wait still the door is open completely
+			case CloseDoor:
+			{
+				// Close the door and wait until the door is closed
+				if (ReadDoorState(currentElevatorState) != Closed)
+				{
+					SetDoorState(Closed, currentElevatorState);
+				}
+				else
+				{
+					state = MoveLift;
+				}
 
-        break;
-      }
+				break;
+			}
 
 
-      case Trouble:
-      {
-        // Fault condition is not treated
-        break;
-      }
-    }
+			case MoveLift:
+			{
+				// Move cabin to the requested floor
+				if (currentElevatorState != requestedElevatorPosition)
+				{
+					MoveElevator(elevatorDirection, Fast);
+				}
+				else
+				{
+					state = OpenDoor;
+				}
+				
+				break;
+			}
 
-    UpdateDisplay(currentElevatorState);  // Update the 7-Seg. display (lift)
-    SetOutput();               // Send the calculated output values to the ports
-  }
 
-  return (0);
+			case OpenDoor:
+			{
+				// Open the door and wait still the door is open completely
+				SetDoorState(Open, currentElevatorState);
+				if (ReadDoorState(currentElevatorState) == Open)
+				{
+					state = Waiting;
+					ClrIndicatorFloorState(currentElevatorState);
+					ClrIndicatorElevatorState(currentElevatorState);
+				}
+				break;
+			}
+
+
+			case Trouble:
+			{
+				// Fault condition is not treated
+				break;
+			}
+		}
+
+	}
+
+	return (0);
 }
 
 
@@ -137,79 +192,79 @@ int main(void)
 // Convert ButtonType to LiftPosType
 LiftPosType ConvertButtonTypeToLiftPosType (ButtonType button)
 {
-  LiftPosType retVal = None;
+	LiftPosType retVal = None;
 
-  switch (button)
-  {
-    case LiftButton_F0:
-    case FloorButton_F0:
-    {
-      retVal = Floor0;
-      break;
-    }
-    case LiftButton_F1:
-    case FloorButton_F1:
-    {
-      retVal = Floor1;
-      break;
-    }
-    case LiftButton_F2:
-    case FloorButton_F2:
-    {
-      retVal = Floor2;
-      break;
-    }
-    case LiftButton_F3:
-    case FloorButton_F3:
-    {
-      retVal = Floor3;
-      break;
-    }
-    default:
-    {
-      //retVal = None;
-      break;
-    }
-  }
+	switch (button)
+	{
+		case LiftButton_F0:
+		case FloorButton_F0:
+		{
+			retVal = Floor0;
+			break;
+		}
+		case LiftButton_F1:
+		case FloorButton_F1:
+		{
+			retVal = Floor1;
+			break;
+		}
+		case LiftButton_F2:
+		case FloorButton_F2:
+		{
+			retVal = Floor2;
+			break;
+		}
+		case LiftButton_F3:
+		case FloorButton_F3:
+		{
+			retVal = Floor3;
+			break;
+		}
+		default:
+		{
+			//retVal = None;
+			break;
+		}
+	}
 
-  return retVal;
+	return retVal;
 }
 
 // Check if buttons are pressed
 ButtonType CheckKeyEvent ()
 {
-  ButtonType retVal = EmergencyButton;
+	ButtonType retVal = EmergencyButton;
 
-  for (ButtonType key = FloorButton_F3; ((key >= LiftButton_F0) && (retVal == EmergencyButton)); key>>=1)
-  {
-    if (ReadKeyEvent(key) == Pressed)
-    {
-      retVal = key;
-    }
-  }
-  return retVal;
+	for (ButtonType key = FloorButton_F3; ((key >= LiftButton_F0) && (retVal == EmergencyButton)); key>>=1)
+	{
+		if (ReadKeyEvent(key) == Pressed)
+		{
+			retVal = key;
+		}
+	}
+	return retVal;
 }
 
 // Update the 7-Seg. display
 void UpdateDisplay (LiftPosType elevatorState)
 {
-  switch (elevatorState)
-  {
-    case Floor0:
-    case Floor1:
-    case Floor2:
-    case Floor3:
-    case Error:
-    case Test:
-    {
-      SetDisplay(elevatorState);
-      break;
-    }
-    default:
-    {
-      break;
-    }
-  }
+	switch (elevatorState)
+	{
+		case Floor0:
+		case Floor1:
+		case Floor2:
+		case Floor3:
+		case Error:
+		case Test:
+		{
+			SetDisplay(elevatorState);
+			break;
+		}
+		default:
+		{
+			break;
+		}
+	}
 }
 
 
